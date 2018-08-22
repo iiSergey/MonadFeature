a330c4b00cf4c41541cb8daecc36560f955026e2
s.ivashchenko <s.ivashchenko@viseven.com>
2018-08-22 18:17:44 +0300

    update ( Program.fs ) : Added State monad

--- MonadFeature/Program.fs
+++ MonadFeature/Program.fs
@@ -1,127 +1,126 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Threading.Tasks
 
-
-type Io<'a> = IO of (unit -> 'a)
-
-//io monad run
-let run (IO thunk) = thunk()
-let comb io1 io2 = IO (fun () -> run io1; run io2)
-
-//io monad bind
-let (>>=) (m:Io<'a>) (fn: 'a -> Io<'b>): Io<'b> = 
-    IO(fun () -> run <| fn (run m ))
-
-type IoMonadBuilder() =
-    //member this.Run (m) = run m
-    member this.Return(t) = IO(fun () -> t)
-    member this.ReturnFrom(t):Io<_> = t
-    member this.Bind(m, fn) = (>>=) m fn
-    member this.Delay(g) : Io<_> = g ()
-    member this.Combine(io1, io2) = comb io1 io2
+module IOMonad =
+    type IO<'a> = IO of (unit -> 'a)
     
-let io =IoMonadBuilder()
+    module IO =
+        let run (IO thunk) = thunk()
+        let combine io1 io2 = IO (fun () -> run io1; run io2)
+        let bind m fn = IO(fun () -> run <| fn (run m ))
+        let map io f = IO(fun () -> f (run io))
+    
+        let (>>=) = bind
 
+    type IoMonadBuilder() =
+        //member this.Run (m) = run m
+        member this.Return(t) = IO(fun () -> t)
+        member inline this.ReturnFrom(t):IO<_> = t
+        member this.Bind(m, fn) = IO.bind m fn
+        member this.Delay(g) : IO<_> = g ()
+        member this.Combine(io1, io2) = IO.combine io1 io2
+        member this.Zero () = IO(fun () -> ())
+    
+    let io =IoMonadBuilder()
+
+module StateMonad =
+    type State<'s, 'a> = State of ('s -> ('a * 's))
+
+    module State =
+        let Run (State f) state  = f state
+        let Return x = State(fun s -> x, s)
+        let Zero = State(fun s -> (), s)
+        let Bind f xState = 
+            let run state = 
+                let x, newState = Run xState state
+                Run (f x) newState
+            State run
+        let Combine xStateLeft xStateRight =
+            //todo: review
+            let run state = 
+                let _result, newState = Run xStateLeft state 
+                Run xStateRight newState
+            State run
+        let Get = 
+            let run state = 
+                state, state
+            State run
+        let Put newState = 
+            let run _ = 
+                (), newState
+            State run
+
+    type StateBuilder() =
+        member this.Zero() = State.Zero
+        member this.Return(x) = State.Return x
+        member inline this.ReturnFrom (x: State<'s, 'a>) = x
+        member this.Bind(xState,f) = State.Bind f xState
+        member this.Combine(xStateLeft, xStateRight) = State.Combine xStateLeft xStateRight
+        
+    let state = new StateBuilder()
+    
+open IOMonad
+open StateMonad
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+
+    let readkeyIO = io { 
+        let! keyConsole = IO (fun () -> Console.ReadKey(false))
+        return keyConsole.Key
+        }
+
+    let printfnIO state= io {
+        do! IO(fun () -> printfn "%s" state)
+    }  
     
-    let step1play _ =
-        printfn "Hello from step 1!"
-        let readkeyIO = fun () -> Console.ReadKey().Key
-        let printfnIO state = fun () -> printfn "%s" state
-    
-        printfn "before readkey press key..."
-        let key=readkeyIO ()
-        printfn "after readkey key %A" key
-
-        let rec play moveIO riderIO state:string =
-            (riderIO "press key up and down. For exit press ESC")()
-            (riderIO state)()
-            match moveIO () with
-            |ConsoleKey.UpArrow ->play moveIO riderIO "+";
-            |ConsoleKey.DownArrow ->play moveIO riderIO "-"; 
-            |ConsoleKey.Escape -> state; 
-            |_ ->play moveIO riderIO "?"; 
-        play readkeyIO printfnIO
-
-    (step1play 1) "--------------" |> ignore
-    
-    let step2play _=
-        printfn "Hello from step 2!"
-        let readkeyIO = IO (fun () -> Console.ReadKey().Key)
-        let readkeyToStringIO key = IO (fun () -> sprintf "%A" key)
-        let printfnIO state = IO(fun () -> printfn "%s" state)
+    let move drawIO =
+        state {
+            let! board = State.Get
+            let key = IO.run drawIO 
+            let st= match key with
+            |ConsoleKey.UpArrow ->"+";
+            |ConsoleKey.DownArrow ->"-"; 
+            |ConsoleKey.Escape -> "exit!"; 
+            |_ ->"?"; 
+            return! State.Put (board + st)
+        }
+    let moveUp () =
+        state {
+            let! board = State.Get
+            return! State.Put (board + "+")
+        }
+ 
+    let moveDown () =
+        state {
+            let! board = State.Get
+            return! State.Put (board + "-")
+        }
         
-        
-        let getAndPrint = readkeyIO >>= readkeyToStringIO >>= printfnIO
-        printfn "run getAndPrint  1 press key..."
-        run getAndPrint
-        printfn "run getAndPrint  2 press key..."
-        run getAndPrint
-        
-        printfn "before readkey press key..."
-        let key= run readkeyIO
-        printfn "after readkey key %A" key
+    let Read info =
+        state {
+            let! board = State.Get
+            return info + board
+        }
 
-        let rec play moveIO riderIO state:string =
-            run (riderIO "press key up and down. For exit press ESC")
-            run (riderIO state)
-            match run moveIO with
-            |ConsoleKey.UpArrow ->play moveIO riderIO "+";
-            |ConsoleKey.DownArrow ->play moveIO riderIO "-"; 
-            |ConsoleKey.Escape -> state; 
-            |_ ->play moveIO riderIO  "?"; 
-        play readkeyIO printfnIO
-
-    (step2play 1) "--------------" |> ignore
+    let draw drawIO =
+        state {
+            let! res = Read "play: "
+            IO.run (drawIO res)
+        }
     
     
-    let step3play _=
-        printfn "Hello from step 3!"
+    //let aa:State<string,string>= play ()
+    //let aaa=State.Run aa "!"
+    
+         
+    let rec game moveIO drawIO state:State<_,string> =    
+        let _,drawState =State.Run (draw drawIO) state
+        let _,newState=State.Run (move moveIO) drawState
+        game moveIO drawIO newState
+
+    game readkeyIO printfnIO "!" |> ignore
         
-        let readkeyIO = IO (fun () -> Console.ReadKey().Key)
-        let readkeyToStringIO key = IO (fun () -> sprintf "%A" key)
-        let printfnIO state = IO(fun () -> printfn "%s" state)
-
-        let getAndPrint = io { 
-            let! key = readkeyIO
-            let! keyString = readkeyToStringIO key
-            let keyString2 = keyString + keyString
-            let! _ = printfnIO keyString2
-            return! printfnIO keyString2
-            }
-        let getAndPrint2 = io {
-            let! key1 = readkeyIO
-            let! key2 = readkeyIO
-            let! keyString1 = readkeyToStringIO key1
-            let! keyString2 = readkeyToStringIO key2
-            return! printfnIO keyString1
-            return! printfnIO keyString2
-        }  
-        printfn "run getAndPrint  1 press key..."
-        printfn "%A" (run <| getAndPrint)
-        
-        printfn "run getAndPrint  2 press key..."
-        printfn "%A" (run <| getAndPrint2)
-
-        printfn "run getAndPrint  3 press key..."
-        printfn "%A" (run <| getAndPrint2)
-        
-        printfn "before fake readkey press key..."
-        let key= run (io { return "FAKE" })
-        printfn "after fake readkey key %A" key
-
-        let rec play moveIO riderIO state:string =
-            run (riderIO "press key up and down. For exit press ESC")
-            run (riderIO state)
-            match run moveIO with
-            |ConsoleKey.UpArrow ->play moveIO riderIO "+";
-            |ConsoleKey.DownArrow ->play moveIO riderIO "-"; 
-            |ConsoleKey.Escape -> state; 
-            |_ ->play moveIO riderIO  "?"; 
-        play readkeyIO printfnIO
-
-    (step3play 1) "--------------" |> ignore
     0 // return an integer exit code
